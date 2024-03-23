3b95ce18d931f4727db5b050c81a0bb1949d16c4
ManurajAg <manu.ag002@gmail.com>
2024-03-24 01:45:19 +0530

    Chat Room designed

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -5,8 +5,18 @@ function connect(){
 	stompClient = Stomp.over(socket);
 	stompClient.connect({},function(frame){
 		console.log("Connected : "+frame);
-		$("#name-form").addClass('d-none')
+		$("#name-form").addClass('d-none');
+		$("#chat-room").removeClass('d-none');
+		
+		//subscribe :- As soon as user enters in chat room he is now subscribed to recieve messages, since url to subscribe to messages is /topic/return-to , thus we used it here
+		stompClient.subscribe("/topic/return-to",function(response){
+			showMessage(JSON.parse(response.body));
+		});
 	})
+}
+
+function showMessage(message){
+	$("#message-container-table").append(`<tr><td><b>${message.name} : </b>${message.content}</td></tr>`)
 }
 
 $(document).ready((e)=>{
